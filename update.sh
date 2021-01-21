c4f2539fdc5f6fd007481acc4f293f78ce546b9f
mayone <mayone321@gmail.com>
2021-01-22 00:03:24 +0800

    [Change] Update update script

--- update.sh
+++ update.sh
@@ -6,13 +6,24 @@
 ZINIT_HOME="$HOME/.zinit"
 
 main() {
+  update_rust
   update_homebrew
   update_zinit
+}
+
+update_rust() {
+  if !check_cmd rustup; then
+    info "No rust for update"
+  fi
+
+  info "Update rust"
+  rustup update
 }
 
 update_homebrew() {
   if ! check_cmd brew; then
     info "No brew for update"
+    return
   fi
 
   info "Update homebrew"
